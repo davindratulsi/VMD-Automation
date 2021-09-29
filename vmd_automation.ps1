bee8cf946028ce215a093e50db775404ebcef812
Davindra Tulsi <50631178+davindratulsi@users.noreply.github.com>
2021-09-29 18:43:08 -0400

    automated looping through files in directory

--- vmd_automation.ps1
+++ vmd_automation.ps1
@@ -1,12 +1,15 @@
 # Powershell execution script
-## powershell -ExecutionPolicy Bypass "& ""C:\Users\Davindra\Desktop\Research\Scripts\vmd\automation\vmd_automation.ps1"""
+## powershell -ExecutionPolicy Bypass "& ""C:\Users\Davindra\Desktop\Research\Scripts\vmd\VMD-Automation\vmd_automation.ps1"""
 
-$sequences = 'A10', 'A9B1', 'A8B2', 'A7B3', 'A6B4',
-'A5B5', 'A4B6', 'A3B7', 'A2B8', 'A1B9'
+# # Specify path 
+$path_sequences = 'C:\Users\Davindra\Desktop\Research\aggregation_study\vesicles_with_loops\'
+$files = Get-ChildItem $path_sequences
+# # Can manually specify list of files/sequences to study or automate it as demonstrated here
 
-Foreach($sequence in $sequences)
+Foreach($file in $files)
 {
- $file = 'C:\Users\Davindra\Desktop\Research\Stiffness_Study\standard_fene\eps_1.5_k_3.0\trajectory_GA_10mers_eps_1.5_k_3.0_standardfene_route0_000_' + $sequence + '_T1000_10000.lammpstrj'
- & "C:\Program Files (x86)\University of Illinois\VMD\vmd.exe" $file -e tcl_render.tcl -args $sequence
+ $sequence = $file.FullName.replace($path_sequences, "")
+ $sequence = $sequence.replace("trajectory_GA_", "")
+ $sequence = $sequence.replace("_T1000_10000.lammpstrj", "")
+ & "C:\Program Files (x86)\University of Illinois\VMD\vmd.exe" $file.FullName -e tcl_render.tcl -args $sequence
 }
-
